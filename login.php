71f1b437f74866901eaf4b9bf42cd091bc6e2126
Jordan-hub77 <jordanjeremiah777@gmail.com>
2025-05-12 19:58:16 +0300

    Update login.php
    nimeandika foti

--- login.php
+++ login.php
@@ -79,7 +79,7 @@ echo (isset($_p[1])) ? ucwords(str_replace("_", " ",$_p[1])) : ucwords(str_repla
       <div class="modal-body">
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+        <button type="button" class="btn btn-primary foti" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
       </div>
       </div>
